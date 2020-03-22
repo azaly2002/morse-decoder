899b3bd724ab5392a68c79979fb71fdccfdf5593
azaly2002 <60570866+azaly2002@users.noreply.github.com>
2020-03-22 17:15:19 +0600

    solved

--- src/index.js
+++ src/index.js
@@ -38,9 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morse_string = '';
+    let result = ''; 
+    let a = expr.split('');
+    for (let i = 1; i < expr.length; i++) {
+        if (a[i] == '0' && a[i - 1] == '1') {
+            morse_string += '.';
+            if (i % 10 == 9) {
+                morse_string += ',';
+            }
+            i += 1;
+        }
+        else (a[i] == '1' && a[i - 1] == '1') {
+            morse_string += '-';
+            if (i % 10 == 9) {
+                morse_string += ',';
+            }
+            i += 1;
+        }
+        else if (a[i] == '*' && a[i - 1] == '*') {
+            morse_string += ' ,';
+            if (i % 10 == 9) {
+                morse_string += ',';
+            }
+            i += 9;
+        }
+        else if (a[i] == '*' && a[i - 1] != '*') {
+            morse_string += ' ,';
+            if (i % 10 == 9) {
+                morse_string += ',';
+            }
+            i += 10;
+        }
+        else if (a[i] == '0' && a[i - 1] == '0') {
+            if (i % 10 == 9) {
+                morse_string += ',';
+            }
+            i += 1;
+        }
+    }
+    let morse_words = morse_string.split(',');
+    for (let j = 0; j < morse_words.length - 1; j++) {
+        if (morse_words[j] == ' '){
+            result += ' ';
+            j++;
+        }
+        result +=  MORSE_TABLE[morse_words[j]];
+    }
+    return result;
 }
-
 module.exports = {
     decode
 }
